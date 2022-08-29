fa05d6399aec74f90d6a0aa2e780c870ce692479
HP <HP@DESKTOP-ESAT5E6>
2022-08-29 10:16:36 +0530

    commited

---
+++ src/test/java/sample/My2Test.java
@@ -0,0 +1,43 @@
+package sample;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class My2Test {
+	WebDriver driver;
+	@BeforeClass
+	public void precondition()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterClass
+	public void postcondition()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void ogibibologin()
+	{
+		driver.get("https://www.goibibo.com/");
+	}
+	
+	@Test
+	public void flipkartlogin()
+	{
+		driver.get("https://www.flipkart.com/");
+	}
+
+}
+
